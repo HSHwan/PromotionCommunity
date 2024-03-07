84980488936715c3382b6ccf678765b3f5f26ba2
HSHwan <tnghks2866@gmail.com>
2024-03-07 20:11:16 +0900

    Updated: Init Spring Project

---
+++ src/main/java/com/promotion_community/PromotionCommunity/PromotionCommunityApplication.java
@@ -0,0 +1,13 @@
+package com.promotion_community.PromotionCommunity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PromotionCommunityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PromotionCommunityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/promotion_community/PromotionCommunity/PromotionCommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.promotion_community.PromotionCommunity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PromotionCommunityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
